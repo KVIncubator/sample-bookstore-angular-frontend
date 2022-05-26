e6f175ce76e7b203f87931703aceb77ff0710841
Dima Denysenko <coding@dimdnk.com>
2022-05-26 13:43:58 +0300

    docs: prepare descriptive info to achieve GitHub community standards

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('basics-angular-bookstore');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('basics-angular-bookstore app is running!');
-  });
 });
